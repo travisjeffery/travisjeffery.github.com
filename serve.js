0b78051ae1e9a4d1e9b0d40c7632488fc2777fab
Travis Jeffery <tj@travisjeffery.com>
2016-02-01 05:49:40 -0500

    add fonts

--- serve.js
+++ serve.js
@@ -65,10 +65,10 @@ metalsmith(__dirname)
     pattern: '**/*.html',
     default: 'post.html'
   }))
-  // .use(browserSync({
-  //   server: 'build',
-  //   files : ['src/**/*', 'layouts/**/*', 'partials/**/*']
-  // }))
+  .use(browserSync({
+    server: 'build',
+    files : ['src/**/*', 'layouts/**/*', 'partials/**/*']
+  }))
   .build(function(err) {
     if (err) throw err
   });
